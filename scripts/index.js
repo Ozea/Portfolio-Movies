9a386a26b450b5d18b53c699849fc0e455f13dae
Alexander <alexander@MacBook-Pro-Alexander.local>
2020-09-09 17:02:54 +0300

    Hamburger instead of arrow in nav. Animation for squeezing sidebar.

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const IMG_ORIGINAL_URL = 'https://image.tmdb.org/t/p/original';
 const CARDS_WRAPPER_COMPONENT = document.getElementById("cards-wrapper");
 const TV_SHOW_DETAILS_COMPONENT = document.getElementById("tv-show-details");
 const MOVIE_DETAILS_COMPONENT = document.getElementById("movie-details");
+const SIDEBAR_COMPONENT = document.getElementById("sidebar");
 let BASE_URL = 'https://api.themoviedb.org/3/';
 let token = 'e4fc8c7613a133ce29f2d207a9bdd7fc';
 let exampleURL = `${BASE_URL}movie/550?api_key=${token}`;
@@ -13,4 +14,8 @@ const displayInitialBrowsePage = () => {
   TV_SHOW_DETAILS_COMPONENT.innerHTML = '';
   MOVIE_DETAILS_COMPONENT.style.display = 'none';
   MOVIE_DETAILS_COMPONENT.innerHTML = '';
+}
+
+const squeezeSidebar = () => {
+  SIDEBAR_COMPONENT.classList.toggle("squeezed");
 }
